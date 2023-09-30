653f1505e2067a3e9704c2a40f159f18cce710fc
Víctor Arreaga <arreagav17@gmail.com>
2023-09-30 11:30:48 -0500

    Deleted `StartController`

--- src/main/java/org/example/mastermind/controllers/ControllerVisitor.java
+++ src/main/java/org/example/mastermind/controllers/ControllerVisitor.java
@@ -1,8 +1,6 @@
 package org.example.mastermind.controllers;
 
 public interface ControllerVisitor {
-    void visit(StartController controller);
-
     void visit(PlayController controller);
 
     void visit(ResumeController controller);
