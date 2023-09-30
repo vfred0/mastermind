79aaca6cd1e56f3ea78de52658fa01950c2b286a
Víctor Arreaga <arreagav17@gmail.com>
2023-09-30 11:26:32 -0500

    States for `play`

---
+++ src/main/java/org/example/mastermind/types/PlayStateValue.java
@@ -0,0 +1,5 @@
+package org.example.mastermind.types;
+
+public enum PlayStateValue {
+    IN_GAME, RESUME, FINISH;
+}
--- src/main/java/org/example/mastermind/types/StateValue.java
+++
@@ -1,5 +0,0 @@
-package org.example.mastermind.types;
-
-public enum StateValue {
-    INITIAL, IN_SETTINGS, IN_GAME, RESUME, FINISH
-}
