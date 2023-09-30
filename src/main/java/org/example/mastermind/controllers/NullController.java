5564c8a669a3dd6912204ef0a9f3a8f6f6074989
Víctor Arreaga <arreagav17@gmail.com>
2023-09-30 11:29:32 -0500

    Added `NullController`

--- src/main/java/org/example/mastermind/controllers/NullController.java
+++ src/main/java/org/example/mastermind/controllers/NullController.java
@@ -3,14 +3,14 @@ package org.example.mastermind.controllers;
 import org.example.mastermind.models.Game;
 import org.example.mastermind.models.State;
 
-public class StartController extends Controller {
+public class NullController extends Controller {
 
-    public StartController(State state, Game game) {
+    public NullController(State state, Game game) {
         super(state, game);
     }
 
     @Override
     public void accept(ControllerVisitor controllerVisitor) {
-        controllerVisitor.visit(this);
+
     }
 }
